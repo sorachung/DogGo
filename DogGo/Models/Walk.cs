98392cc6111fa2825db7ff61961303cbff313bea
sorachung <sorachung.src@gmail.com>
2022-03-30 09:35:31 -0500

    created Walk model

---
+++ DogGo/Models/Walk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DogGo.Models
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public int WalkerId { get; set; }
+        public int DogId { get; set; }
+        public Walker Walker { get; set; }
+        public Dog Dog { get; set; }
+
+    }
+}
